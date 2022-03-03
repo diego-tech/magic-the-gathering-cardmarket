eda4d8079600d5f51a7c8c085174f878641997a4
Diego Muñoz Herranz <diego171200@gmail.com>
2022-03-03 19:26:46 +0100

    Add Card Test and Comments

--- tests/Feature/CardTest.php
+++ tests/Feature/CardTest.php
@@ -4,6 +4,7 @@ namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Laravel\Sanctum\Sanctum;
 use Tests\TestCase;
 
 class CardTest extends TestCase
@@ -13,10 +14,82 @@ class CardTest extends TestCase
      *
      * @return void
      */
-    public function test_example()
+    public function test_noAuth()
     {
-        $response = $this->get('/');
+        $data = [
+            "name" => "Carta Test",
+            "description" => "Esto es una carta de test",
+            "collection" => 1
+        ];
 
-        $response->assertStatus(200);
+        $api_token = '';
+
+        $response = $this->withHeaders([
+            'Authorization' => 'Bearer ' . $api_token,
+            'Accept' => 'application/json'
+        ])->postJson('/api/registerCard', $data);
+
+        $response->assertStatus(401);
+    }
+
+    public function test_noData()
+    {
+        $data = [
+            "name" => "",
+            "description" => "",
+            "collection" => null
+        ];
+
+        $api_token = '10|lkCjeus4eUXaaTn648kbl0uAeP7iZlkmJ0axWsvE';
+
+        $response = $this->withHeaders([
+            'Authorization' => 'Bearer ' . $api_token,
+            'Accept' => 'application/json'
+        ])->postJson('/api/registerCard', $data);
+
+        $response->assertStatus(400)->assertJson([
+            'status' => 0,
+            'msg' => 'Ha ocurrido un error.'
+        ]);
+    }
+
+    public function test_badCollectionId() {
+        $data = [
+            "name" => "Carta Test",
+            "description" => "Esto es una carta de test",
+            "collection" => 4
+        ];
+
+        $api_token = '10|lkCjeus4eUXaaTn648kbl0uAeP7iZlkmJ0axWsvE';
+
+        $response = $this->withHeaders([
+            'Authorization' => 'Bearer ' . $api_token,
+            'Accept' => 'application/json'
+        ])->postJson('/api/registerCard', $data);
+
+        $response->assertStatus(400)->assertJson([
+            'status' => 0,
+            'msg' => 'Ha ocurrido un error.'
+        ]);
+    }
+
+    public function test_allDataOk() {
+        $data = [
+            "name" => "Carta Test",
+            "description" => "Esto es una carta de test",
+            "collection" => 1
+        ];
+
+        $api_token = '10|lkCjeus4eUXaaTn648kbl0uAeP7iZlkmJ0axWsvE';
+
+        $response = $this->withHeaders([
+            'Authorization' => 'Bearer ' . $api_token,
+            'Accept' => 'application/json'
+        ])->postJson('/api/registerCard', $data);
+
+        $response->assertStatus(200)->assertJson([
+            'status' => 1,
+            'msg' => 'Carta Guardada Correctamente'
+        ]);
     }
 }
